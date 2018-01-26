2c7bed5ac180bd0abbc04b8788c9a6fd4a3386e1
Goran <gorpetric@foi.hr>
2018-01-26 15:55:18 +0100

    Update dependencies & fb link

--- js/app/home/components/Home.vue
+++ js/app/home/components/Home.vue
@@ -10,7 +10,7 @@
                 <a target='_blank' href='https://github.com/gorpetric' title='Github'><i class='fa fa-github'></i></a>
                 <a target='_blank' href='http://codepen.io/goranp/' title='Codepen'><i class='fa fa-codepen'></i></a>
                 <a target='_blank' href='https://twitter.com/gogichill' title='Twitter'><i class='fa fa-twitter'></i></a>
-                <a target='_blank' href='https://www.facebook.com/yotriix' title='Facebook'><i class='fa fa-facebook'></i></a>
+                <a target='_blank' href='https://www.facebook.com/q0r4n' title='Facebook'><i class='fa fa-facebook'></i></a>
                 <a target='_blank' href='https://www.youtube.com/yotrixx' title='Youtube'><i class='fa fa-youtube'></i></a>
                 <a target='_blank' href='https://www.instagram.com/gogichill' title='Instagram'><i class='fa fa-instagram'></i></a>
             </div>
